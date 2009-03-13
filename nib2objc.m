460a2e319dba030aac706a898f3dd4bbeb292fa3
Adrian Kosmaczewski <akosma@gmail.com>
2009-03-13 17:30:25 +0100

    Reorganized the code, added comments

--- nib2objc.m
+++ nib2objc.m
@@ -1,22 +1,10 @@
 #import <Foundation/Foundation.h>
 
-NSDictionary *getPropertyList(NSData *data)
-{
-    NSString *errorStr = nil;
-    NSPropertyListFormat format;
-    NSDictionary *propertyList = [NSPropertyListSerialization propertyListFromData:data
-                                                                  mutabilityOption:NSPropertyListImmutable
-                                                                            format:&format
-                                                                  errorDescription:&errorStr];
-    [errorStr release];
-    return propertyList;
-}
+NSDictionary *getPropertyList(NSData *data);
+NSDictionary *getDictionaryFromNIB(NSString *nibFile);
+NSString *getText(NSData *data);
 
-NSString *getText(NSData *data)
-{
-    return [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
-}
-
+// Call this program using "build/Debug/nib2objc ../../TestViewController.xib" during development.
 int main (int argc, const char * argv[]) 
 {
     NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
@@ -38,7 +26,33 @@ int main (int argc, const char * argv[])
         NSLog(@"This utility requires a valid NIB file path as parameter.");
         return 0;
     }
+    
+    NSDictionary *dict = getDictionaryFromNIB(nibFile);
+    NSLog(@"items: %d", [[dict objectForKey:@"com.apple.ibtool.document.hierarchy"] count]);
 
+    [pool drain];
+    return 0;
+}
+
+NSDictionary *getPropertyList(NSData *data)
+{
+    NSString *errorStr = nil;
+    NSPropertyListFormat format;
+    NSDictionary *propertyList = [NSPropertyListSerialization propertyListFromData:data
+                                                                  mutabilityOption:NSPropertyListImmutable
+                                                                            format:&format
+                                                                  errorDescription:&errorStr];
+    [errorStr release];
+    return propertyList;
+}
+
+NSString *getText(NSData *data)
+{
+    return [[[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding] autorelease];
+}
+
+NSDictionary *getDictionaryFromNIB(NSString *nibFile)
+{
     // Build the NSTask that will run the ibtool utility
     NSArray *arguments = [NSArray arrayWithObjects:nibFile, @"--objects", 
                           @"--hierarchy", @"--connections", @"--classes", nil];
@@ -52,19 +66,18 @@ int main (int argc, const char * argv[])
     [task setArguments:arguments];
     [task setStandardOutput:pipe];
     [task launch];
-
+    
     while ((data = [readHandle availableData]) && [data length]) 
     {
         [output appendData:data];
     }
-
+    
     // This dictionary is ready to be parsed, and it contains
     // everything we need from the NIB file.
     NSDictionary *dict = getPropertyList(output);
-    NSLog(@"items: %d", [[dict objectForKey:@"com.apple.ibtool.document.hierarchy"] count]);
-
+    
     [output release];
     [task release];
-    [pool drain];
-    return 0;
+    
+    return dict;
 }

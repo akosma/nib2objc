1965a17d8212ec83540ccb1b0ebff28f85f1029f
Adrian Kosmaczewski <akosma@gmail.com>
2010-07-17 11:15:28 +0200

    Added support for the UIImageView.highlighted property

--- Processors/UIImageViewProcessor.m
+++ Processors/UIImageViewProcessor.m
@@ -7,6 +7,7 @@
 //
 
 #import "UIImageViewProcessor.h"
+#import "NSNumber+Nib2ObjcExtensions.h"
 
 @implementation UIImageViewProcessor
 
@@ -22,10 +23,20 @@
 
 - (void)processKey:(id)item value:(id)value
 {
-    // The image property cannot be retrieved by ibtool, 
-    // so here we just add the property with a nil value.
-    [output setObject:@"nil" forKey:@"image"];
-    [super processKey:item value:value];
+    if ([item isEqualToString:@"image"])
+    {
+        // The image property cannot be retrieved by ibtool, 
+        // so here we just add the property with a nil value.
+        [output setObject:@"nil" forKey:@"image"];
+    }    
+    else if ([item isEqualToString:@"highlighted"])
+    {
+        [output setObject:[value booleanString] forKey:item];
+    }
+    else
+    {
+        [super processKey:item value:value];
+    }
 }
 
 @end

d2e627594f624dadd8953a83628f6c81cbcbecc9
Adrian Kosmaczewski <akosma@gmail.com>
2010-07-17 09:30:13 +0200

    Changed copyright texts in some files

--- Processors/UITabBarItemProcessor.m
+++ Processors/UITabBarItemProcessor.m
@@ -3,7 +3,7 @@
 //  nib2objc
 //
 //  Created by Adrian on 3/17/09.
-//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//  Adrian Kosmaczewski 2009
 //
 
 #import "UITabBarItemProcessor.h"

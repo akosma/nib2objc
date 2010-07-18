3269fb13951c52a36bbd13dcc08a7e54bd937965
Adrian Kosmaczewski <akosma@gmail.com>
2010-07-18 12:57:25 +0200

    Added an application delegate, to avoid having an untitled window on app startup

---
+++ GUI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  nib2objc
+//
+//  Created by Adrian on 7/18/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+{
+
+}
+
+@end
---
+++ GUI/AppDelegate.m
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.m
+//  nib2objc
+//
+//  Created by Adrian on 7/18/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+
+@implementation AppDelegate
+
+- (BOOL)applicationShouldOpenUntitledFile:(NSApplication *)sender
+{
+    return NO;
+}
+
+@end
